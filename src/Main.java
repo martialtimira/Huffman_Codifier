1266882bec02de93110ebbf07e82c8612243fc8c
martialtimira <zignar1999@gmail.com>
2023-04-04 12:41:15 +0200

    Created Pair.java and prob_table.csv

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Pair<String, Integer>> prob_list = new ArrayList<Pair<String, Integer>>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader("prob_table.csv"))) {
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         System.out.println("Hello world!");
     }
 }
