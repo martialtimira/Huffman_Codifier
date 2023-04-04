a4047f50397beb92129485b36c90beece60db590
Joan <joan.joan425@gmail.com>
2023-04-04 15:24:47 +0200

    Comparador Comparador d'ArrayList de Pairs

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
 
         ArrayList<Pair<String, Float>> prob_list = new ArrayList<>();
+
 
         try(BufferedReader br = new BufferedReader(new FileReader("Input_Files/prob_table.csv"))) {
             String line;
@@ -18,12 +19,18 @@ public class Main {
                 System.out.println("Pair Created: [" + element.getFirst() + ", " + element.getSecond() + "]");
                 prob_list.add(element);
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
         System.out.println("LIST SIZE: " + prob_list.size());
+
+        sort(prob_list);
+
     }
+
+    private static void sort(ArrayList<Pair<String, Float>> l) {
+        l.sort(Comparator.comparing(Pair::getSecond));
+    }
+
 }
